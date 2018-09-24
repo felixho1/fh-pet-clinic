7194728832b4f0ac56992b3313bc920233a3bdab
Felix Ho <felixho@gmail.com>
2018-09-24 14:52:06 +1000

    Initial Add

---
+++ src/main/java/ho/felix/fhpetclinic/FhPetClinicApplication.java
@@ -0,0 +1,12 @@
+package ho.felix.fhpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhPetClinicApplication.class, args);
+    }
+}
