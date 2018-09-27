ef017392ce178d8db27612d8d7f2683fce684c00
Felix Ho <felix.ho@nab.com.au>
2018-09-28 06:50:56 +1000

    List all Owners on Owners Index Page. Closes #14.

--- pet-clinic-web/src/main/java/ho/felix/fhpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ho/felix/fhpetclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package ho.felix.fhpetclinic.controllers;
 
+import ho.felix.fhpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("all_owners", ownerService.findAll());
         return "owners/index";
     }
 }
