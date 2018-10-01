a25e43d341af843777858e408bdcc808bb214a24
Felix Ho <felix.ho@nab.com.au>
2018-10-01 13:53:09 +1000

    Create Vet Speciality Entity, Associate To Vet. Closes #19

---
+++ pet-clinic-data/src/main/java/ho/felix/fhpetclinic/model/Speciality.java
@@ -0,0 +1,12 @@
+package ho.felix.fhpetclinic.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/ho/felix/fhpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ho/felix/fhpetclinic/model/Vet.java
@@ -1,4 +1,14 @@
 package ho.felix.fhpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
