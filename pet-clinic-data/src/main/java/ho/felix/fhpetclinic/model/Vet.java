887a18db7007bab75d9ad2e82b268939440476a2
Felix Ho <felix.ho@nab.com.au>
2018-10-02 11:58:34 +1000

    Convert Vets to JPA Entities. Closes #29

--- pet-clinic-data/src/main/java/ho/felix/fhpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ho/felix/fhpetclinic/model/Vet.java
@@ -1,9 +1,18 @@
 package ho.felix.fhpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @OneToMany
+    @JoinTable(
+            name = "vet_specialities",
+            joinColumns = { @JoinColumn(name = "vet_id") },
+            inverseJoinColumns = { @JoinColumn(name = "speciality_id") }
+    )
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
