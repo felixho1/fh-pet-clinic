c7ab17d41cbdc6f4d69bd848f037d46c908a9e76
Felix Ho <felix.ho@nab.com.au>
2018-10-02 12:36:46 +1000

    Add Visit in DataLoader. Closes #37

--- pet-clinic-web/src/main/java/ho/felix/fhpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ho/felix/fhpetclinic/bootstrap/DataLoader.java
@@ -14,14 +14,16 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, PetService petService, VetService vetService,
-                      PetTypeService petTypeService, SpecialityService specialityService) {
+                      PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.petService = petService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -85,6 +87,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
